685648a6338fc15efe28c16f344cdfef7dd6311d
Adrien Masson <adrien.masson@hotmail.fr>
2023-01-19 20:58:50 +0100

    [DEV] Functional test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
+import App, { replaceCamelWithSpaces } from "./App";
 
 test("button has correct initial color", () => {
 	render(<App />);
@@ -67,4 +67,20 @@ test("Clicked disabled button has gray background and revert to blue", () => {
 	expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
 	fireEvent.click(checkbox);
 	expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+});
+
+describe("spaces before camel-case capital letters", () => {
+	test("Works for no inner capital letters", () => {
+		expect(replaceCamelWithSpaces("Red")).toBe("Red");
+	});
+
+	test("Works for one inner capital letters", () => {
+		expect(replaceCamelWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+	});
+
+	test("Works for many inner capital letters", () => {
+		expect(replaceCamelWithSpaces("MediumVioletRed")).toBe(
+			"Medium Violet Red"
+		);
+	});
 });
